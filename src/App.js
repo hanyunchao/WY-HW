d2ae3a9c6fb6f34d14802f99e34fcf55a001f377
hanyunchao <hanyunchao0808@163.com>
2019-10-28 21:13:23 +0800

    r7-react

--- src/App.js
+++ src/App.js
@@ -2,31 +2,44 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import confirm from './confirm';
 
 class InputNumber extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props)
-    this.state = {value: this.props.value || this.props.defaultValue || '请输入内容'}
+    this.state = { value: this.props.value || this.props.defaultValue || '请输入内容' }
   }
-  handle(e){
+
+  // 最后一题
+  async componentDidMount() {
+    let res = await confirm("确定删除吗")
+    if (res) {
+      console.log("是")
+    } else {
+      console.log("否")
+    }
+  }
+
+  handle(e) {
     this.props.onChange()
     this.setState({
-      value:e.target.value
+      value: e.target.value
     })
   }
-  render(){
-    return (<input value={this.state.value} onChange={(e)=>{this.handle(e)}}></input>)
+  render() {
+    return (<input value={this.state.value} onChange={(e) => { this.handle(e) }}></input>)
   }
 }
 
-function App(){
-  const [value,setValue] = useState('aaa')
+function App() {
+  const [value, setValue] = useState('aaa')
   // const value = 'hyc'
   return (
-      <div>
-      <InputNumber value={value} onChange={e=>{console.log(1)}}/>
-      <InputNumber defaultValue={value} onChange={e=>{}}/>
-      </div>
+    <div>
+
+      <InputNumber value={value} onChange={e => { console.log(1) }} />
+      <InputNumber defaultValue={value} onChange={e => { }} />
+    </div>
   )
 }
 
---
+++ src/confirm.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { resolve } from 'upath';
+import ReactDOM from 'react-dom';
+
+class Modal extends React.Component {
+
+    onOk(){
+        alert(this.props.message)
+        this.props.resolve(true)
+    }
+    ondelete(){
+        alert(this.props.message)
+        this.props.resolve(false)
+    }
+
+    render(){
+        return(
+            <div>
+                <button onClick={()=>{this.onOk()}}>确认</button>
+                <button onClick={()=>{this.ondelete()}}>取消</button>
+            </div>
+        )
+    }
+}
+
+
+function confirm(message){
+
+    if(Object.prototype.toString.call(message) === "[Object String]"){
+
+
+    }
+
+    let promise = new Promise((resolve,reject)=>{
+        let modaldiv = document.createElement("div")
+        modaldiv.className = "ant-modal-mask"
+        document.body.appendChild(modaldiv)
+        ReactDOM.render(<Modal resolve={resolve} message={message}/>, document.getElementsByClassName('ant-modal-mask')[0]);
+    })
+    
+
+
+    return promise
+}
+
+export default confirm;
